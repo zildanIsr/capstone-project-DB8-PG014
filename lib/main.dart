fafc210ae82774ee346c4304b721ffc06242a772
zildanIsr <zildan.isrezki@gmail.com>
2025-10-08 09:16:02 +0700

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:finmene/firebase_options.dart';
 import 'package:finmene/providers/theme_provider.dart';
 import 'package:finmene/utils/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
