7b1df17601369fd844dbdfe36e4d0253c9500bb3
H.P.P <59654979+Dgranfather@users.noreply.github.com>
2025-10-30 23:04:42 +0700

    Create main.dart
    main untuk contoh

---
+++ ML_Doc/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'forecast/monthly_forecaster.dart';
+
+late final MonthlyForecaster forecaster;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  forecaster = MonthlyForecaster();
+  await forecaster.init(); // load & allocate model sekali
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Forecast Demo',
+      home: const ForecastPage(),
+    );
+  }
+}
